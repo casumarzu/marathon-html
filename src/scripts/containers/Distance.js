26b4fa9de097dd77a81e85a5817fb8afb42d2168
andrey <casumarzu@yandex.ru>
2016-07-15 12:21:29 +0300

    Change distance view

--- src/scripts/containers/Distance.js
+++ src/scripts/containers/Distance.js
@@ -3,16 +3,19 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import _ from 'lodash'
 import { Router, Route, Link, browserHistory } from 'react-router'
-import { Card, CardHeader, CardTitle, CardText, Divider, CircularProgress, RaisedButton } from 'material-ui'
+import { Card, CardHeader, CardTitle, CardText, Divider, CircularProgress, RaisedButton, Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui'
+import { secondsToTime } from 'Util'
 
 import ParticipantList from 'Components/participants/ParticipantList'
 import * as participantsActions from 'Actions/Participants.Actions'
 
 import * as distancesActions from 'Actions/Distances.Actions'
+import * as sprintsActions from 'Actions/Sprints.Actions'
 
 import Registration from 'Components/sprints/Registration'
 
 import s from 'Styles/index.styl'
+
 
 class Distance extends Component {
   constructor(props) {
@@ -23,7 +26,9 @@ class Distance extends Component {
     const { props } = this
     const { race_id, distance_id } = props.params
     const { index, add } = props.participantsActions
+    const { show } = props.sprintsActions
     index(race_id, distance_id)
+    show(race_id)
   }
 
   render() {
@@ -31,25 +36,70 @@ class Distance extends Component {
     const { race_id, distance_id } = props.params
     const { index, add } = props.participantsActions
     const { participants } = props
+    const { status, start_date_time } = props.sprint
 
-    let RegistrationNode = ''
-    // if(type === 'present') {
-    //   RegistrationNode = <Registration race_id={race_id} distance_id={distance_id} add={ add } />
-    // }
-    RegistrationNode = <Registration race_id={race_id} distance_id={distance_id} add={ add } />
+    let RegistrationNode = '',
+        RegistrationHeaderNode = ''
+    if(status === 'future') {
+      RegistrationNode = <Registration race_id={race_id} distance_id={distance_id} add={ add } />
+      RegistrationHeaderNode = (
+        <div>
+          <h1>Дистанция</h1>
+        </div>
+      )
+    }
 
     let ParticipantsNode = ''
     if(participants.length){
-      ParticipantsNode = <ParticipantList list={ participants } />
+      if(status === 'future') {
+        ParticipantsNode = <ParticipantList list={ participants } />
+      }else {
+
+        const participantsSorted = _.orderBy(participants, ['result'], ['asc'])
+        const ParticipantsListNode = participantsSorted.map( (participant, i) => {
+          const { id, name, result } = participant
+          let run_result = 'Нет результата'
+          if(result && start_date_time) {
+            const resultTime = new Date(result).getTime()
+            const startDateTime = new Date(start_date_time).getTime()
+            const totalSec = ( resultTime - startDateTime ) / 1000
+            run_result = secondsToTime(totalSec)
+          }
+          return (
+            <TableRow key={id}>
+              <TableRowColumn>{++i}</TableRowColumn>
+              {/*<TableRowColumn>{id}</TableRowColumn>*/}
+              <TableRowColumn>{name}</TableRowColumn>
+              <TableRowColumn>{run_result}</TableRowColumn>
+            </TableRow>
+          )
+        })
+
+        ParticipantsNode = (
+          <div>
+            <h1>Результаты</h1>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHeaderColumn>Место</TableHeaderColumn>
+                  {/*<TableHeaderColumn>ID</TableHeaderColumn>*/}
+                  <TableHeaderColumn>Имя</TableHeaderColumn>
+                  <TableHeaderColumn>Результат</TableHeaderColumn>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {ParticipantsListNode}
+              </TableBody>
+            </Table>
+          </div>
+        )
+      }
     }
     const backUrl = () => browserHistory.push(`/sprint/${race_id}`)
     return (
       <div>
         <RaisedButton label="Назад" primary={true} onTouchTap={ backUrl } />
-
-        <div>
-          <h1>Дистанция</h1>
-        </div>
+        {RegistrationHeaderNode}
         <div>
           <div className={s.FlexItem} style={{flexGrow: 1}}>{ RegistrationNode }</div>
           <div className={s.FlexItem} style={{flexGrow: 1}}>{ ParticipantsNode }</div>
@@ -67,14 +117,16 @@ Distance.propTypes = {
 function mapStateToProps(state) {
   return {
     distance: state.distances.item,
-    participants: state.participants.list
+    participants: state.participants.list,
+    sprint: state.sprints.item
   }
 }
 
 function mapDispatchToProps(dispatch) {
   return {
     participantsActions: bindActionCreators(participantsActions, dispatch),
-    distancesActions: bindActionCreators(distancesActions, dispatch)
+    distancesActions: bindActionCreators(distancesActions, dispatch),
+    sprintsActions: bindActionCreators(sprintsActions, dispatch),
   }
 }
 
